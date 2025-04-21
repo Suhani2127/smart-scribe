eb1a8cd084da9257bfe553f3cd84d7e408d8e3be
Suhani2127 <155729934+Suhani2127@users.noreply.github.com>
2025-04-21 22:13:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import PyPDF2
 import requests
 
 # 🔐 Hugging Face API setup
-HUGGINGFACE_API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased"
+HUGGINGFACE_API_URL = "https://api-inference.huggingface.co/models/t5-small"
 
 headers = {"Authorization": f"Bearer {st.secrets['HUGGINGFACE_API_KEY']}"}
 
